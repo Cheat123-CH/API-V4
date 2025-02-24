f877af233fc25b8dd26b3da79a2f5ac174c1cc30
camcyber <camcyber@sophat.local>
2025-02-24 13:49:19 +0700

    modify dashboard

--- src/app/resources/r3-admin/a1-dashbord/controller.ts
+++ src/app/resources/r3-admin/a1-dashbord/controller.ts
@@ -17,8 +17,11 @@ export class DashboardController {
         @Query('yesterday') yesterday?: string,
         @Query('thisWeek') thisWeek?: string,
         @Query('thisMonth') thisMonth?: string,
+        @Query('threeMonthAgo') threeMonthAgo?: string,
+        @Query('sixMonthAgo') sixMonthAgo?: string,
+        @Query('type') type?: number,
     ) {
-        return await this._service.findStaticData({ today, yesterday, thisWeek, thisMonth });
+        return await this._service.findStaticData({ today, yesterday, thisWeek, thisMonth, threeMonthAgo, sixMonthAgo, type });
     }
     @Get('/cashier')
     async getCashierAndTotalSale(
