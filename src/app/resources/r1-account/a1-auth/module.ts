1086d971d85b35cc664b1956a648c37c77a9ae52
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-05-09 10:12:09 +0700

    fix: add forFeature models to AuthModule

--- src/app/resources/r1-account/a1-auth/module.ts
+++ src/app/resources/r1-account/a1-auth/module.ts
@@ -1,14 +1,20 @@
-// ===========================================================================>> Core Library
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-// ===========================================================================>> Costom Library
 import { EmailService } from '@app/services/email.service';
 import { AuthController } from './controller';
 import { AuthService } from './service';
 
+import User from '@app/models/user/user.model';
+import UserOTP from '@app/models/user/user_otps.model';
+import UserRoles from '@app/models/user/user_roles.model';
+import UsersLogs from '@app/models/user/user_logs.model';
+
 @Module({
+    imports: [
+        SequelizeModule.forFeature([User, UserOTP, UserRoles, UsersLogs])
+    ],
     controllers: [AuthController],
     providers: [AuthService, EmailService]
 })
-
 export class AuthModule { }
